23c880612265b272da4866b5a7b58c2c96709a2c
Priyanka Chaudhuri <priankac@udel.edu>
2026-05-11 15:01:19 -0400

    RevealAnswer

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,19 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [isVariable, setVariable] = useState<boolean>(false);
+    return (
+        <div>
+            <Button
+                onClick={() => {
+                    setVariable((currentValue) => !currentValue);
+                }}
+            >
+                Reveal Answer
+            </Button>
+            {isVariable ?
+                <p>42</p>
+            :   null}
+        </div>
+    );
 }
