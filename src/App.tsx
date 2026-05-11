82351ae2cc796400edaa4b88dc486f7851c601ce
Priyanka Chaudhuri <priankac@udel.edu>
2026-05-11 14:09:23 -0400

    Save changes before task-state merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,61 @@
 import React from "react";
+import { Button, Container, Row, Col } from "react-bootstrap";
+import animal from "./animals/shutterstock_2068599128-1.jpg.avif";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript Priyanka Chaudhuri
+                <h1>
+                    UD CISC275 with React Hooks and TypeScript Priyanka
+                    Chaudhuri Hello World
+                </h1>
             </header>
+            <img src={animal} alt="Animal" width={300} />
+
+            <div>Animals</div>
+            <ul>
+                <li>Lion</li>
+                <li>Wombat</li>
+                <li>Frog</li>
+            </ul>
+            <Button
+                variant="primary"
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <Container style={{ marginTop: "2rem" }}>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "red",
+                            }}
+                        />
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "red",
+                            }}
+                        />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
