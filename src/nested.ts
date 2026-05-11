acb7eb2cda226b60d0b7227ec8ffdd4d88400570
Priyanka Chaudhuri <priankac@udel.edu>
2026-05-11 01:27:18 -0400

    thirteenth

--- src/nested.ts
+++ src/nested.ts
@@ -120,7 +120,10 @@ export function publishAll(questions: Question[]): Question[] {
  * are the same type. They can be any type, as long as they are all the SAME type.
  */
 export function sameType(questions: Question[]): boolean {
-    return false;
+    if (questions.length === 0) {
+        return true;
+    }
+    return questions.every((ask) => ask.type === questions[0].type);
 }
 
 /***
