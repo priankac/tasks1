44046a4a99e04d5c13bf1262e4ea1a60ba44e0b2
Priyanka Chaudhuri <priankac@udel.edu>
2026-05-11 00:34:23 -0400

    ninth

--- src/objects.ts
+++ src/objects.ts
@@ -134,7 +134,11 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    const add_option = [...question.options, newOption];
+    return {
+        ...question,
+        options: add_option,
+    };
 }
 
 /**
