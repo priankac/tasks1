5791c242b94e532346a6fa1c46b32973c381da1d
Priyanka Chaudhuri <priankac@udel.edu>
2026-05-11 14:49:08 -0400

    counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Button, Container, Row, Col } from "react-bootstrap";
-import animal from "./animals/shutterstock_2068599128-1.jpg.avif";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { ChangeType } from "./components/ChangeType";
@@ -14,54 +12,19 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>
-                    UD CISC275 with React Hooks and TypeScript Priyanka
-                    Chaudhuri Hello World
-                </h1>
+                <h1>UD CISC275 with React Hooks and TypeScript</h1>
             </header>
-            <img src={animal} alt="Animal" width={300} />
-
-            <div>Animals</div>
-            <ul>
-                <li>Lion</li>
-                <li>Wombat</li>
-                <li>Frog</li>
-            </ul>
-            <Button
-                variant="primary"
-                onClick={() => {
-                    console.log("Hello World!");
-                }}
-            >
-                Log Hello World
-            </Button>
-
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
-            <Container style={{ marginTop: "2rem" }}>
-                <Row>
-                    <Col>
-                        <div
-                            style={{
-                                width: "100px",
-                                height: "100px",
-                                backgroundColor: "red",
-                            }}
-                        />
-                    </Col>
-                    <Col>
-                        <div
-                            style={{
-                                width: "100px",
-                                height: "100px",
-                                backgroundColor: "red",
-                            }}
-                        />
-                    </Col>
-                </Row>
-            </Container>
+            <Counter />
+            <hr />
+            <RevealAnswer />
+            <hr />
+            <StartAttempt />
+            <hr />
+            <TwoDice />
+            <hr />
+            <ChangeType />
+            <hr />
+            <CycleHoliday />
         </div>
     );
 }
