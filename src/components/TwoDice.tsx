c70fe5f319d2da61bc4c343a0d9990bf801d9904
Priyanka Chaudhuri <priankac@udel.edu>
2026-05-11 15:55:42 -0400

    TwoDice

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,28 @@ export function d6(): number {
 }
 
 export function TwoDice(): React.JSX.Element {
-    return <div>Two Dice</div>;
+    const [leftButton, setLeftButton] = useState<number>(1);
+    const [rightButton, setRightButton] = useState<number>(2);
+    function leftDie(): void {
+        setLeftButton(d6());
+    }
+    function rightDie(): void {
+        setRightButton(d6());
+    }
+    let score = "";
+    if (leftButton === 1 && rightButton === 1) {
+        score = "Lose";
+    } else if (leftButton === rightButton) {
+        score = "Win";
+    }
+    return (
+        <div>
+            <h3>Two Dice</h3>
+            <div data-testid="left-die">Left Die: {leftButton}</div>
+            <div data-testid="right-die">Right Die: {rightButton}</div>
+            <Button onClick={leftDie}>Roll Left</Button>
+            <Button onClick={rightDie}>Roll Right</Button>
+            <div>{score}</div>
+        </div>
+    );
 }
