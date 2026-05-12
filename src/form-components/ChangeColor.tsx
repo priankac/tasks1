11b63b060d2bda779b73a2beee9b75d2d71a77c7
Priyanka Chaudhuri <priankac@udel.edu>
2026-05-12 00:11:01 -0400

    ChangeColor

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,38 @@
 import React, { useState } from "react";
 
 export function ChangeColor(): React.JSX.Element {
+    const colors: string[] = [
+        "yellow",
+        "red",
+        "blue",
+        "white",
+        "orange",
+        "violet",
+        "black",
+        "purple",
+    ];
+    const [color, setColor] = useState<string>("red");
+    function updateWheel(event: React.ChangeEvent<HTMLInputElement>): void {
+        setColor(event.target.value);
+    }
     return (
         <div>
             <h3>Change Color</h3>
+            {colors.map((indColor: string) => (
+                <label key={indColor}>
+                    <input
+                        type="radio"
+                        name="color"
+                        value={indColor}
+                        checked={color === indColor}
+                        onChange={updateWheel}
+                    />
+                    {indColor}
+                </label>
+            ))}
+            <div data-testid="colored-box" style={{ backgroundColor: color }}>
+                You have chosen {color}.
+            </div>
         </div>
     );
 }
--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -5,9 +5,15 @@ export function CheckAnswer({
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [enteredAnswer, setEnteredAnswer] = useState<string>("");
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>): void {
+        setEnteredAnswer(event.target.value);
+    }
     return (
         <div>
             <h3>Check Answer</h3>
+            <input type="text" value={enteredAnswer} onChange={updateAnswer} />
+            {enteredAnswer === expectedAnswer ? "✔️" : "❌"}
         </div>
     );
 }
